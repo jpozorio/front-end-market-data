d50f414f50078aff15b184de8c1e6240fddb919f
João Paulo Ozório da Silva <jpozorio@gmail.com>
2020-07-02 23:26:01 -0300

    calculadora de ir

--- src/app/ir-calculator/ir-calculator.component.ts
+++ src/app/ir-calculator/ir-calculator.component.ts
@@ -1,3 +1,4 @@
+import {HttpClient} from '@angular/common/http';
 import {Component} from '@angular/core';
 
 @Component({
@@ -7,4 +8,31 @@ import {Component} from '@angular/core';
 })
 export class IrCalculatorComponent {
 
+  fileToUpload: File = null;
+  brutoDayTrade: 0.00;
+  liquidoDayTrade: 0.00;
+  impostoRetido: 0.00;
+  impostoTotal: 0.00;
+
+  constructor(public httpClient: HttpClient) {
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  postFile(fileToUpload: File): void {
+    const endpoint = '/market-data/ir-calculator/calculate';
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    this.httpClient
+        .post(endpoint, formData, {}).subscribe((resp: any) => {
+        this.brutoDayTrade = resp.brutoDayTrade;
+        this.liquidoDayTrade = resp.liquidoDayTrade;
+        this.impostoRetido = resp.impostoRetido;
+        this.impostoTotal = resp.impostoTotal;
+      },
+    )
+    ;
+  }
 }
