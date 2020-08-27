09ea132b67bca84e5ef33136900db98fb3f2817c
João Paulo Ozório da Silva <jpozorio@gmail.com>
2020-08-26 21:25:45 -0300

    importa mat-dialog-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
@@ -74,6 +75,7 @@ export function getToken(): string {
     FormsModule,
     MatInputModule,
     MatSnackBarModule,
+    MatDialogModule,
   ],
   providers   : [
     {
